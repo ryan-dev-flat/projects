f6d587d2e5880f1ad25536912699ad3446bd61c6
Ryan <coap.ryanmontgomery@gmail.com>
2024-07-26 13:33:54 -0600

    initial commit

---
+++ server/migrations/versions/25a3264dee33_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 25a3264dee33
+Revises: 
+Create Date: 2024-07-26 13:33:32.259050
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '25a3264dee33'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
